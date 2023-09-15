7d7587795411b4dd9c5fe6774714e3e60e07b7e2
Melqui Brito <Merkulino11@gmail.com>
2023-09-15 18:24:31 -0300

    Init project, add Company model

---
+++ Models/Company.cs
@@ -0,0 +1,11 @@
+public class Company
+{
+  public string CNPJ { get; set; }
+  public string name { get; set; }
+  public string about { get; set; }
+  public string address { get; set; }
+  public string city { get; set; }
+  public string phone { get; set; }
+  public string status { get; set; }
+  public string shareCapital { get; set; }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Grup_GPS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
