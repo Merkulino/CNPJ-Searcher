498ca7f4635d2715c1cdac5e9f5fbc9152155dbb
Melqui Brito <Merkulino11@gmail.com>
2023-09-15 22:00:16 -0300

    Get Request from API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,46 @@ namespace Grup_GPS.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly HttpClient httpClient;
+  public HomeController()
+  {
+    httpClient = new HttpClient();
+  }
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+  [HttpGet("SearchCompany")]
+  public async Task<ViewResult> SearchCompany(string cnpj)
+  {
+    try
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      Console.Write(cnpj);
+      string response = await httpClient.GetStringAsync($"https://receitaws.com.br/v1/cnpj/{cnpj}");
+      Console.Write(response);
     }
+    catch (System.Exception)
+    {
+      throw;
+    }
+    return View();
+  }
+
+  public void SaveCompany()
+  {
+
+  }
 }
